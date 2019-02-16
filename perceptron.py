5488382c2c450227c96c2fa4d1cbafade2c49ab2
Randy Cahya Wihandika <rendicahya@gmail.com>
2019-02-16 13:02:33 +0700

    Added Perceptron

--- perceptron.py
+++ perceptron.py
@@ -1,10 +1,13 @@
 import numpy as np
 
 from draw_utils import line, plot
-from functions import percep_step
 
 
-def percep_train(s, t, th=0, alpha=1, draw=False):
+def step(y, th=0):
+    return 1 if y > th else 0 if -th <= y <= th else -1
+
+
+def percep_train(s, t, th=0, a=1, draw=False):
     w = np.zeros(len(s[0]) + 1)
     b = np.ones((len(s), 1))
     s = np.hstack((b, s))
@@ -19,11 +22,11 @@ def percep_train(s, t, th=0, alpha=1, draw=False):
 
         for r, row in enumerate(s):
             y_in = np.dot(row, w)
-            y_out = percep_step(y_in, th)
+            y_out = step(y_in, th)
 
             if y_out != t[r]:
                 stop = False
-                w = [w[i] + alpha * t[r] * row[i] for i in range(len(row))]
+                w = [w[i] + a * t[r] * row[i] for i in range(len(row))]
 
             print('Bobot: {}'.format(w))
 
