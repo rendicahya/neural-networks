ad1e214298b3cb1fbbff9b01952e9b6823c7acaf
Randy Cahya Wihandika <rendicahya@gmail.com>
2019-02-16 13:00:20 +0700

    Added Maxnet

---
+++ maxnet.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+def act(x):
+    return x if x >= 0 else 0
+
+
+def iter(a, e=None):
+    if e is None:
+        e = np.random.uniform(0, len(a))
+
+    while np.count_nonzero(a) > 1:
+        a_new = np.zeros(len(a))
+
+        for i in range(len(a)):
+            s = sum([a[j] for j in range(len(a)) if j != i])
+            a_new[i] = act(a[i] - e * s)
+
+        a = a_new
+
+        print(a)
+
+
+def main():
+    a = [.2, .4, .6, .8]
+    e = .2
+    iter(a, e)
+
+
+if __name__ == '__main__':
+    main()
