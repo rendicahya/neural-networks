6a74463e044db3ddab0a266089106984b8a2367a
Randy Cahya Wihandika <rendicahya@gmail.com>
2019-02-17 17:25:08 +0700

    Edited Mexican hat

--- mexican_hat.py
+++ mexican_hat.py
@@ -5,7 +5,7 @@ def act(x):
     return [0 if i < 0 else i if 0 <= i <= 2 else 2 for i in x]
 
 
-def iter(x, r2, c1, c2, t_max):
+def run(x, r2, c1, c2, t_max):
     k = [c1] * (r2 * 2 + 1)
     k[0] = k[-1] = c2
 
@@ -16,7 +16,7 @@ def iter(x, r2, c1, c2, t_max):
 
 def main():
     x = [-1, .5, .8, 1, .8, .5, -1]
-    iter(x, 2, .6, -.4, 5)
+    run(x, 2, .6, -.4, 5)
 
 
 if __name__ == '__main__':
