74751292bfb51ae7b73cb2bfcfe8122a4ae5c4a4
Randy Cahya Wihandika <rendicahya@gmail.com>
2019-03-04 07:03:06 +0700

    Nothing

--- perceptron.py
+++ perceptron.py
@@ -1,13 +1,14 @@
 import numpy as np
 
-from draw_utils import line, plot
+
+# from draw_utils import line, plot
 
 
-def step(y, th=0):
+def step(y, th=0.):
     return 1 if y > th else 0 if -th <= y <= th else -1
 
 
-def percep_train(s, t, th=0, a=1, draw=False):
+def percep_train(s, t, th=0., a=1, draw=False):
     w = np.zeros(len(s[0]) + 1)
     b = np.ones((len(s), 1))
     s = np.hstack((b, s))
@@ -30,7 +31,30 @@ def percep_train(s, t, th=0, a=1, draw=False):
 
             print('Bobot: {}'.format(w))
 
-            if draw:
-                plot(line(w, th), line(w, -th), s, t)
+            # if draw:
+            #     plot(line(w, th), line(w, -th), s, t)
 
     return w
+
+
+def percep_test(x, w, th=0):
+    y_in = w[0] + np.dot(x, w[1:])
+
+    return step(y_in, th)
+
+
+def main():
+    s = [[1, 1, 1],
+         [1, -1, 1],
+         [-1, 1, -1],
+         [-1, -1, 1]]
+    t = [1, 1, -1, -1]
+
+    w = percep_train(s, t, .5)
+    y = percep_test([1, 1, -1], w)
+
+    print(y)
+
+
+if __name__ == '__main__':
+    main()
