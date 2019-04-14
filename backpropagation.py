2f341545bfa2ae39b23e5b81f469655dcb64f02c
Randy Cahya Wihandika <rendicahya@gmail.com>
2019-04-14 21:12:39 +0700

    Progress: backpropagation

--- backpropagation.py
+++ backpropagation.py
@@ -1,25 +1,76 @@
-import math
 import numpy as np
+from sklearn.datasets import load_iris
 
 
-def sigmoid(x):
-    return 1 / (1 + math.exp(-x))
+def sig(X):
+    return [1 / (1 + np.exp(-x)) for x in X]
 
 
-def bp_train(arch):
-    w1 = np.random.rand(arch[0], arch[1])
-    dw1 = np.random.rand(arch[0], arch[1])
+def sigd(X):
+    for i, x in enumerate(X):
+        s = sig([x])[0]
 
-    w2 = np.random.rand(arch[1], arch[2])
-    dw2 = np.random.rand(arch[1], arch[2])
+        yield s * (1 - s)
 
-    in1 = np.zeros(arch[0], np.float64)
-    in2 = np.zeros(arch[1], np.float64)
-    in3 = np.zeros(arch[2], np.float64)
+
+def bp_train(C, X, t, a, mep, mer):
+    nin = [np.empty(c) for c in C]
+    n = [np.empty(c + 1) if i < len(C) - 1 else np.empty(c) for i, c in enumerate(C)]
+    w = [np.random.rand(C[i] + 1, C[i + 1]) for i in range(len(C) - 1)]
+    dw = [np.empty((C[i] + 1, C[i + 1])) for i in range(len(C) - 1)]
+    d = [np.empty(s) for s in C[1:]]
+    din = [np.empty(s) for s in C[1:-1]]
+    ep = 0
+    er = []
+    mse = 1
+
+    w = np.array([[[.1, .2],
+                   [.3, .4],
+                   [.5, .6],
+                   [.7, .8]],
+                  [[.1, .4],
+                   [.2, .5],
+                   [.3, .6]]])
+
+    for i in range(0, len(n) - 1):
+        n[i][-1] = 1
+
+    while ep < mep and mse > mer:
+        ep += 1
+
+        for r in range(len(X)):
+            n[0][:-1] = X[r]
+
+            for L in range(1, len(C)):
+                nin[L] = np.dot(n[L - 1], w[L - 1])
+                n[L][:len(nin[L])] = sig(nin[L])
+
+            e = t[r] - n[-1]
+            d[-1] = e * list(sigd(nin[-1]))
+            dw[-1] = a * d[-1] * n[-2].reshape((-1, 1))
+
+            for L in range(len(C) - 1, 1, -1):
+                din[L - 2] = np.dot(d[L - 1], np.transpose(w[L - 1][:-1]))
+                d[L - 2] = din[L - 2] * np.array(list(sigd(nin[L - 1])))
+                dw[L - 2] = (a * d[L - 2]) * n[L - 2].reshape((-1, 1))
+
+            w += dw
+
+    return w
 
 
 def bp_test():
     pass
 
 
-bp_train((2, 3, 2))
+r = 3, 2, 2
+# iris = load_iris()
+# X = iris.data
+# y = iris.target
+X = [[.8, .2, .1],
+     [.1, .8, .9]]
+y = [[0, 0],
+     [0, 1]]
+W = bp_train(r, X, y, .1, 1, .1)
+
+print(W)
---
+++ coba.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+a = [1, 2, 3]
+
+print(np.reshape(a, 1))
