fcbb595ba01771ac9b5ab45790c050374c545539
Randy Cahya Wihandika <rendicahya@gmail.com>
2019-04-20 22:13:31 +0700

    Nothing

--- coba.py
+++ coba.py
@@ -1,10 +1,5 @@
 import numpy as np
 
-p = np.array([[0, 0], [0, 1], [1, 0]])
-r = [[0.0600547, 0.75866121],
-     [0.95049736, 0.17046602],
-     [0.00596853, 0.15488992]]
+a = [1, 2]
 
-d = [np.argmin(np.sum(abs(i - p), axis=1)) for i in r]
-
-print(d)
+print(a[::-1])
