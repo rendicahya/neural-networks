f98314e84fc2a4acb61163f9a9537964dd3a7893
Randy Cahya Wihandika <rendicahya@gmail.com>
2019-04-20 11:11:47 +0700

    Refactoring and adding comments

--- functions.py
+++ functions.py
@@ -1,6 +1,20 @@
-def binstep(x, th=0):
+def binstep(x, th=0) -> int:
+    """
+    Thresholding a value either to 1 or 0 based on the threshold value.
+
+    :param x: The input value
+    :param th: The threshold value
+    :return: The thresholding result (0 or 1)
+    """
     return 1 if x >= th else 0
 
 
-def bipstep(y, th=0):
-    return 1 if y >= th else -1
+def bipstep(x, th=0) -> int:
+    """
+    Thresholding a value either to 1 or -1 based on the threshold value.
+
+    :param x: The input value
+    :param th: The threshold value
+    :return: The thresholding result (-1 or 1)
+    """
+    return 1 if x >= th else -1
