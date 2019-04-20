e4fd7c02386810547f706db4b57cb424879f2ae3
Randy Cahya Wihandika <rendicahya@gmail.com>
2019-04-20 16:58:13 +0700

    Removing comments

--- functions.py
+++ functions.py
@@ -1,20 +1,6 @@
 def binstep(x, th: int = 0) -> int:
-    """
-    Thresholding a value either to 1 or 0 (binary) based on the threshold value.
-
-    :param x: The input value
-    :param th: The threshold value
-    :return: The thresholding result (0 or 1)
-    """
     return 1 if x >= th else 0
 
 
 def bipstep(x, th: int = 0) -> int:
-    """
-    Thresholding a value either to 1 or -1 (bipolar) based on the threshold value.
-
-    :param x: The input value
-    :param th: The threshold value
-    :return: The thresholding result (-1 or 1)
-    """
     return 1 if x >= th else -1
