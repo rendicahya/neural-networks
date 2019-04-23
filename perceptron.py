1e35dde95f20da86cde6db3dcdda6c82280282ea
Randy Cahya Wihandika <rendicahya@gmail.com>
2019-04-23 20:15:44 +0700

    Finishing test

--- perceptron.py
+++ perceptron.py
@@ -7,8 +7,8 @@ from sklearn.preprocessing import minmax_scale
 from draw_utils import line, plot
 
 
-def step(y, th=0.):
-    return 1 if y > th else 0 if -th <= y <= th else -1
+def step(x, th):
+    return 1 if x > th else 0 if -th <= x <= th else -1
 
 
 def percep_train(s, t, th=0., a=1, draw=None):
@@ -19,12 +19,12 @@ def percep_train(s, t, th=0., a=1, draw=None):
     s = np.hstack((s, b))
     stop = False
     epoch = 0
-    # w = np.arange(1, 9).reshape((4, 2))
 
     while not stop:
         stop = True
         epoch += 1
-        print(f'epoch: {epoch}')
+
+        # print(epoch)
 
         for i, s_ in enumerate(s):
             for j, t_ in enumerate(t[i]):
@@ -36,44 +36,36 @@ def percep_train(s, t, th=0., a=1, draw=None):
                     dw = a * t_ * s_
                     w[:, j:j + 1] += dw.reshape(nx + 1, -1)
 
-                # if draw == 'loop':
-                #     plot([line(w, th), line(w, -th)], s, t)
+                if draw == 'loop' and nx == 2:
+                    plot([line(w, th), line(w, -th)], s, t)
 
-        # print(w)
-        # if draw == 'result':
-        #     plot([line(w, th), line(w, -th)], s, t)
+        if draw == 'result' and nx == 2:
+            plot([line(w, th), line(w, -th)], s, t)
 
     return w, epoch
 
 
 def percep_test(X, w, th=0):
     for x in X:
-        y_in = w[0] + np.dot(x, w[1:])
+        y_in = np.dot([*x, 1], w)
+        y = [step(i, th) for i in y_in]
 
-        yield step(y_in, th)
+        yield y
 
 
 def test():
-    # s = [[1, -1, 1],
-    #      [1, -1, 1],
-    #      [-1, 1, -1],
-    #      [-1, -1, 1]]
-    # t = [1, 1, -1, -1]
+    s = [[1, 1],
+         [1, 0],
+         [0, 1],
+         [0, 0]]
 
-    # s = [[1, -1, -1],
-    #      [-1, -1, -1],
-    #      [-1, 1, -1]]
-    # t = [-1, 1, 1]
+    t = [[1, 0], [0, 1], [0, 1], [0, 1]]
 
-    s = [[.8, .8, .8],
-         [.2, .2, .2]]
-    t = [[1, 0],
-         [0, 1]]
-
-    w = percep_train(s, t, .5)
+    w, epoch = percep_train(s, t, .2)
     # y = percep_test([-1, 1, -1], w)
 
-    # print(y)
+    print(w)
+    print(epoch)
 
 
 def test_iris():
@@ -81,25 +73,22 @@ def test_iris():
     iris = iris.loc[iris['species'] != 'virginica']
     iris = iris.drop(['sepal_width', 'petal_width'], axis=1)
 
-    # iris.loc[iris['species'] == 'virginica', 'species'] = 0
-    # iris.loc[iris['species'] == 'setosa', 'species'] = 1
-    # iris.loc[iris['species'] == 'versicolor', 'species'] = 2
-
     X = iris[['sepal_length', 'petal_length']].to_numpy()
     X = minmax_scale(X)
-    c = {'virginica': [1, 0, 0], 'setosa': [0, 1, 0], 'versicolor': [0, 0, 1]}
-    y = [c[i] for i in iris['species'].to_numpy()]
+
+    c = {'virginica': [-1, -1], 'setosa': [-1, 1], 'versicolor': [1, -1]}
+    y = iris['species'].to_numpy()
+    y = [c[i] for i in y]
+
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3)
-
-    print(y)
-
     w, epoch = percep_train(X_train, y_train)
-    # res = list(percep_test(X_test, w))
-    # acc = accuracy_score(res, y_test)
 
-    # print(f'Epoch: {epoch}')
-    # print(f'Accuracy: {acc}')
+    out = list(percep_test(X_test, w))
+    acc = accuracy_score(out, y_test)
+
+    print(f'Epoch: {epoch}')
+    print(f'Accuracy: {acc}')
 
 
 if __name__ == '__main__':
-    test()
+    test_iris()
