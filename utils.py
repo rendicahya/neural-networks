db58502e2ea7c0f6f4992d223081046c3dd8132a
Randy Cahya Wihandika <rendicahya@gmail.com>
2019-05-03 20:40:43 +0700

    to_pattern() and to_class()

--- utils.py
+++ utils.py
@@ -1,18 +1,33 @@
-def bin_class(x):
-    length = len(bin(max(x))[2:])
+def to_pattern(X):
+    bin_length = len(bin(max(X))[2:])
+    result = []
 
-    for i in x:
-        b = bin(i)[2:].zfill(length)
+    for i in X:
+        b = bin(i)[2:].zfill(bin_length)
 
-        yield [int(n) for n in b]
+        result.append([int(n) for n in b])
+
+    return result
+
+
+def to_class(pattern):
+    classes = []
+
+    for p in pattern:
+        rounded = [int(round(x)) for x in p]
+        string = ''.join(str(x) for x in rounded)
+        integer = int(string, 2)
+
+        classes.append(integer)
+
+    return classes
 
 
 def test():
-    x1 = bin_class([1, 2, 3])
-    x2 = bin_class([1, 2, 3, 4])
-
-    print(list(x1))
-    print(list(x2))
+    print(to_pattern([1, 2, 3]))
+    print(to_pattern([1, 2, 3, 4]))
+    print(to_class([.1, .9, .1]))
+    print(to_class([0.20232853, 0.67238648]))
 
 
 if __name__ == '__main__':
