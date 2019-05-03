029c92372440f74dd76702f392820950c7ad6d5d
Randy Cahya Wihandika <rendicahya@gmail.com>
2019-05-03 12:34:35 +0700

    UCI Digits dataset

---
+++ digits.py
@@ -0,0 +1,23 @@
+import numpy as np
+from backpropagation import bp_fit, bp_predict
+from sklearn import datasets
+from sklearn.preprocessing import minmax_scale
+from sklearn.model_selection import train_test_split
+
+
+def bp():
+    digits = datasets.load_digits()
+    X = minmax_scale(digits.data)
+    y = digits.target
+    Y = np.zeros((len(X), 10), np.uint8)
+    c = 64, 60, 10
+
+    for i in range(len(y)):
+        Y[i, y[i]] = 1
+
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=.3)
+    w, ep, mse = bp_fit(c, X_train, y_train, .1, 1000, .1)
+
+
+if __name__ == '__main__':
+    bp()
