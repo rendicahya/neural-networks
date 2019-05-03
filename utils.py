0dff4833c8feafc016bde1c133b73db848b2e3af
Randy Cahya Wihandika <rendicahya@gmail.com>
2019-05-03 20:01:05 +0700

    Renaming

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-def to_binary_string(x):
+def bin_class(x):
     length = len(bin(max(x))[2:])
 
     for i in x:
@@ -8,8 +8,8 @@ def to_binary_string(x):
 
 
 def test():
-    x1 = to_binary_string([1, 2, 3])
-    x2 = to_binary_string([1, 2, 3, 4])
+    x1 = bin_class([1, 2, 3])
+    x2 = bin_class([1, 2, 3, 4])
 
     print(list(x1))
     print(list(x2))
