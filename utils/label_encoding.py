992e2cd0394f86afb07cd2fcbbc67ca9d5ce8589
Randy Cahya Wihandika <rendicahya@gmail.com>
2019-05-05 22:26:43 +0700

    Binary encoding min value

--- utils/label_encoding.py
+++ utils/label_encoding.py
@@ -2,24 +2,25 @@ import numpy as np
 
 
 def bin_enc(lbl):
-    bin_length = len(bin(max(lbl))[2:])
-    pattern = []
+    mi = min(lbl)
+    length = len(bin(max(lbl) - mi + 1)[2:])
+    enc = []
 
     for i in lbl:
-        b = bin(i)[2:].zfill(bin_length)
+        b = bin(i - mi)[2:].zfill(length)
 
-        pattern.append([int(n) for n in b])
+        enc.append([int(n) for n in b])
 
-    return pattern
+    return enc
 
 
-def bin_dec(enc):
+def bin_dec(enc, mi=0):
     lbl = []
 
     for e in enc:
         rounded = [int(round(x)) for x in e]
         string = ''.join(str(x) for x in rounded)
-        num = int(string, 2)
+        num = int(string, 2) + mi
 
         lbl.append(num)
 
@@ -41,20 +42,22 @@ def onehot_dec(enc, mi=0):
 
 
 def test():
-    # print(binary_pattern([1, 2, 3]))
-    # print(binary_pattern([1, 2, 3, 4]))
-    #
+    print(bin_enc([1, 2, 3]))
+    # print(bin_enc([1, 2, 3, 4]))
+
+    print(bin_dec([[0, 0], [0, 1], [1, 0]], 1))
+
     # print(to_class([[.1, .9, .1]]))
     # print(to_class([[0.20232853, 0.67238648]]))
     #
     # print(onehot_enc([1, 2, 3]))
-    print(onehot_enc([3, 4, 5, 6]))
+    # print(onehot_enc([3, 4, 5, 6]))
     # print(onehot_enc([2, 3]))
 
-    print(onehot_dec([[1, 0, 0, 0],
-                      [0, 1, 0, 0],
-                      [0, 0, 1, 0],
-                      [0, 0, 0, 1]], 3))
+    # print(onehot_dec([[1, 0, 0, 0],
+    #                   [0, 1, 0, 0],
+    #                   [0, 0, 1, 0],
+    #                   [0, 0, 0, 1]], 3))
 
 
 if __name__ == '__main__':
