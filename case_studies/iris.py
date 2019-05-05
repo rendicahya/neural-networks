5b3483bc718e411f4777840d0cf03958402779fe
Randy Cahya Wihandika <rendicahya@gmail.com>
2019-05-05 13:21:34 +0700

    Nothing

--- case_studies/iris.py
+++ case_studies/iris.py
@@ -7,7 +7,7 @@ from sklearn.preprocessing import minmax_scale
 
 from backpropagation import bp_predict, bp_fit
 from elm import elm_fit, elm_predict
-from utils.label_pattern import bin_enc, bin_dec
+from utils.label_pattern import onehot_enc, onehot_dec
 
 
 def plot():
@@ -32,9 +32,9 @@ def plot():
 def bp():
     iris = load_iris()
     X = minmax_scale(iris.data)
-    Y = bin_enc(iris.target)
+    Y = onehot_enc(iris.target)
 
-    c = 4, 3, 2
+    c = 4, 9, 3
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=.3)
     w, ep, mse = bp_fit(c, X_train, y_train, .1, 1000, .1)
 
@@ -42,8 +42,8 @@ def bp():
     print(f'MSE: {mse}')
 
     predict = bp_predict(X_test, w)
-    predict = bin_dec(predict)
-    y_test = bin_dec(y_test)
+    predict = onehot_dec(predict)
+    y_test = onehot_dec(y_test)
     acc = accuracy_score(predict, y_test)
 
     print(f'Output: {predict}')
@@ -54,7 +54,7 @@ def bp():
 def elm():
     iris = load_iris()
     X = minmax_scale(iris.data)
-    Y = bin_enc(iris.target)
+    Y = onehot_enc(iris.target)
 
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=.3)
     W, b, mape = elm_fit(X_train, y_train, 5)
@@ -62,8 +62,8 @@ def elm():
     print(f'MAPE: {mape}')
 
     predict = elm_predict(X_test, W, b)
-    predict = bin_dec(predict)
-    y_test = bin_dec(y_test)
+    predict = onehot_dec(predict)
+    y_test = onehot_dec(y_test)
     acc = accuracy_score(predict, y_test)
 
     print(f'Output: {predict}')
@@ -72,4 +72,4 @@ def elm():
 
 
 if __name__ == '__main__':
-    plot()
+    elm()
